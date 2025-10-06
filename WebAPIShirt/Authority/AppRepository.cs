99d8cb9b3c7ea81aafdcc77be223940136f6484f
Simone Gaboli <s.gaboli@gsoftconsulting.it>
2025-10-06 11:58:52 +0200

    Develop Authority

---
+++ WebAPIShirt/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace WebAPIShirt.Authority
+{
+    public static class AppRepository
+    {
+        private static List<Application> _applications = new List<Application>()
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "E0790E15-01D3-42C9-B4CD-9AA65B12FFE1", //secret e clientId generati da SQl server:
+                Secret = "45C01F42-09C2-49DD-9436-5E50646155B6" //SELECT NEWID() AS ClientId, NEWID() AS Secret
+            }
+        };
+        public static bool Authenticate(string clientId, string secret)
+        {
+            return _applications.Any(a => a.ClientId == clientId && a.Secret == secret);
+        }
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(a => a.ClientId == clientId);
+        }
+
+
+    }
+}
---
+++ WebAPIShirt/Authority/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPIShirt.Authority
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+        public string? ApplicationName { get; set; }
+        public string? ClientId { get; set; }
+        public string? Secret { get; set; }
+    }
+}
